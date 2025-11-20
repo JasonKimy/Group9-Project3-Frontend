3b9ca8b094b86d5c3f567078b9a706b5f7dac8af
Micah <micahheneveld@gmail.com>
2025-11-20 03:12:16 -0800

    I guess I had to still make hrefs but set them to null to hide unwanted screens from showing as tabs.  huhhhhhhhhh

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,6 +45,18 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="CheckInScreen"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="models"
+        options={{
+          href: null,
+        }}
+      />
     </Tabs>
   );
 }
